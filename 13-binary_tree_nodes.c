fc4fefe76f41e4f8f7e918b120c04669cec43de7
alexUd01 <alexanderikpeama@gmail.com>
2023-03-01 21:26:03 +0100

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,8 +9,24 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t l_count = 0, r_count = 0, counted = 0;
 	if (!tree)
 		return (0);
 
-	
+	if (tree->left)
+	{
+		l_count = 1 + binary_tree_nodes(tree->left);
+		/* set a flag to identify counted nodes */
+		counted = 1;
+	}
+	if (tree->right)
+	{
+		/* count right-parent nodes */
+		r_count = 1 + binary_tree_nodes(tree->right);
+		/* subtract 1 from `r_count` if already counted above */
+		if (counted)
+			r_count--;
+	}
+
+	return (l_count + r_count);
 }
