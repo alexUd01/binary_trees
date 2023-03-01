e80c9ac5d995da719fd1a9533a96357c47f94636
alexUd01 <alexanderikpeama@gmail.com>
2023-03-02 00:50:10 +0100

    created task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - a function that finds the sibling of a node
+ * @node: a pointer to the node to find the sibling
+ * Description: if node is NULL or parent is NULL, return NULL.
+ * Return: returns a pointer to the sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	/* check for right sibling */
+	if (node->parent->right && node != node->parent->right)
+		return (node->parent->right);
+
+	/* check for left sibling */
+	if (node->parent->left && node != node->parent->left)
+		return (node->parent->left);
+
+	
+	/* no (left/right) siblings */
+	return (NULL);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -71,10 +71,13 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 
-/* balance-factor ckecker, is-full check,  */
+/* balance-factor-ckecker, is-full-check, is-perfect-check */
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/* find-sibling, find-uncle */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 
 
