d2d3d45a7b8ce8bea4af00d443f7bac04b354202
alexUd01 <alexanderikpeama@gmail.com>
2023-03-02 00:50:34 +0100

    created task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node->parent->left && node != node->parent->left)
 		return (node->parent->left);
 
-	
+
 	/* no (left/right) siblings */
 	return (NULL);
 }
