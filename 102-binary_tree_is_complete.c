9572d6c026018866a3cad6e94d030d0a4d2a3991
alexUd01 <alexanderikpeama@gmail.com>
2023-03-02 14:31:38 +0100

    task 29

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -2,7 +2,8 @@
 
 /*-------------------- UNCOMPLETED PROJECT ------------------------ */
 /**
- * binary_tree_is_complete - a function that ckecks if a binary tree is complete
+ * binary_tree_is_complete - a function that ckecks if a binary tree is
+ *                           complete
  * @tree: a pointer to the root node of the tree to check
  * Return: 1 if complete 0 otherwise
  */
--- binary_trees.h
+++ binary_trees.h
@@ -96,5 +96,17 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 /* Task: 21 is complete */
 int binary_tree_is_complete(const binary_tree_t *tree);
 
+/* rotate */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+
+/* Binary Search Tree */
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
+
+
 
 #endif /* BINARY_TREES_H */
