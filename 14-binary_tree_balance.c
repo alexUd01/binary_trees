6055ccb60134bb758b0477330088e72536ae6d20
alexUd01 <alexanderikpeama@gmail.com>
2023-03-02 11:09:24 +0100

    corrected task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,20 +3,31 @@
 /**
  * binary_tree_balance - a function that measures the balance factor of a
  *                       binary tree
- * @tree: a pointer to the root node of the treeto measure the balance factor
+ * @tree: a pointer to the root node of the tree to measure the balance factor
  * Return: returns the balance factor
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int balance = 0;
+	int bal = 0, l_bal = 0, r_bal = 0;
 
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		balance++;
-	if (tree->right)
-		balance++;
+	/* all leaf-nodes have the value zero (0) as balance factor */
+	if (!tree->left && !tree->right)
+		return (0);
 
-	return (balance);
+	/* left recursive check */
+	if (tree->left)
+		l_bal = 1 + binary_tree_balance(tree->left);
+
+	/* right recursive check */
+	if (tree->right)
+		r_bal = 1 + binary_tree_balance(tree->right);
+
+	/* balance factor is the difference between the left height and */
+	/* right subtrees of a node */
+	bal = l_bal - r_bal;
+
+	return (bal);
 }
